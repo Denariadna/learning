462975e61ab414bff86b55a8ea18a6247dda46f5
denariadna <dar06arina@yandex.ru>
2025-05-05 15:38:39 +0300

    Webpack hot module replacement

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -2,9 +2,10 @@ import { BuildOptions } from "./types/config";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
-    return {
-        port: options.port,
-        open: true, //автоматически открывает страницу браузера с приложением
-        historyApiFallback: true,
-    }
+  return {
+    port: options.port,
+    open: true, //автоматически открывает страницу браузера с приложением
+    historyApiFallback: true,
+    hot: true,
+  };
 }
